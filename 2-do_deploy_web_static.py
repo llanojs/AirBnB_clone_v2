d4190b2d678023a5dd93ae840166e66b0a444b7e
llanojs <juanllano93@gmail.com>
2020-08-18 00:26:12 -0500

    update task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,11 +4,11 @@
     servers
 """
 
-from fabric.api import run, put, sudo
+from fabric.api import run, put, sudo, env
 from os import path
 import datetime
 
-env.hosts = ['<IP web-01>', 'IP web-02']
+env.hosts = ['35.243.226.232', '50.19.157.176']
 
 
 def do_deploy(archive_path):
@@ -20,15 +20,24 @@ def do_deploy(archive_path):
     try:
         print("Executing task 'do_deploy'")
         # Upload file
-        put(archive_path, / tmp/)
+        put(archive_path, /tmp/)
         # Create folder
 	file_name = archive_path.split(".")[0]
-	file_name = "/data/web_static/releases/{}".format(directory)
-	run("mkdir -p {}".format(file_name)
-        # Uncompress
-	run("tar -xzf /tmp/{} -C {}".format(archive_path, file_name)
-        # delete file uploaded from /tmp/ folder
-        #
+	folder = "/data/web_static/releases/{}/".format(file_name)
+	run("mkdir -p {}".format(folder)
+	# Uncompress
+	run("tar -xzf /tmp/{} -C {}".format(archive_path, folder)
+    # delete file uploaded from /tmp/ folder
+	run("rm /tmp/{}".format(archive_path)
+    # move the information to the upper folder
+	run("mv {}/web_static/* {}".format(folder, folder))
+	# delete the simbolic link
+	run("rm -rf {}/web_static".format(folder))
+	run("rm -rf /data/web_static/current")
+	# Create a new the symlink /data/web_static/current on the web server,
+    # linked to the new version ot the code --->
+    # (/data/web_static/releases/<filename without extension>)
+	run("ln -s {} /data/web_static/current".format(folder))
         return(True)
     except BaseException:
         return(False)
